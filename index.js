39c5cb5df15e0dbf64e7d540d68443906992cce6
chris <orthagonal1@gmail.com>
2016-03-25 12:29:46 -0500

    help options, fixed a stray 'function' statement

--- index.js
+++ index.js
@@ -1,45 +1,20 @@
 #!/usr/bin/env node
 'use strict';
 
-const conf = require('./lib/bootstrap');
-const action = conf.action;
-const aws = conf.aws;
-const region = conf.region;
-const argv = conf.argv;
-
-const actions = {
-  groups: (aws, region, argv) => {
-    const groups = require('./lib/logs').groups;
-    groups(aws, region, argv);
-  },
-  streams: (aws, region, argv) => {
-    const streams = require('./lib/logs').streams;
-    streams(aws, region, argv);
-  },
-  tail: (aws, region, argv) => {
-    if (argv.length === 2) {
-      const groups = require('./lib/groups');
-      groups(cwLogs);
-    } else {
-      const logs = require('./lib/tail-logs');
-      const logGroupName = process.env.AWS_GROUP_NAME || process.argv[3];
-      const logStreamName = process.env.AWS_STREAM_NAME || process.argv[4];
-      const cwLogs = new aws.CloudWatchLogs({ region: region });
-      logs(cwLogs, logGroupName, logStreamName);
-    }
-  },
-  search: (aws, region, argv) => {
-
-  },
-  metric: (aws, region, argv) => {
-
-  },
-  help: () => {
-  }
-}
-
-if (actions[action]) {
-  actions[action](aws,region,argv);
-} else {
-  actions.help();
-}
+require('yargs')
+.command(
+  'groups',
+  'list cloudwatch log groups ',
+  require('./lib/groups.js')
+).command(
+  'streams',
+  'list streams for a group',
+  require('./lib/streams.js')
+).command(
+  'tail',
+  'grab the last 10 mins of logs and then fetch every few seconds',
+  require('./lib/tail.js')
+)
+.help('h')
+.alias('h', 'help')
+.argv;
