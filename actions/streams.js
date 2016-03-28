df9879e9edbd90bdbbb3f9a76d84bd109b3f593d
chris <orthagonal1@gmail.com>
2016-03-28 14:56:49 -0500

    reorganized lib/ and actions/ dirs

---
+++ actions/streams.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-console */
+'use strict';
+const Table = require('cli-table');
+const moment = require('moment');
+const filesize = require('filesize');
+const async = require('async');
+const colors = require('colors');
+
+const getStreams = (cwlogs, groups) => {
+  if (!Array.isArray(groups)) {
+    groups = [groups];
+  }
+
+  const table = new Table({
+    head: ['No', 'Group Name', 'Name', 'Created', 'Size']
+  });
+
+  let count = 1;
+
+  async.eachSeries(groups, (group, next) => {
+    cwlogs.describeLogStreams({ logGroupName: group }, (err, data) => {
+      if (err) {
+        return next(err);
+      }
+
+      data.logStreams.forEach((stream) => {
+        const name = stream.logStreamName;
+        const created = moment(stream.creationTime);
+        const size = filesize(stream.storedBytes);
+
+        table.push([count, group, name, created.format('YYYY-MM-DD HH:MM:SS'), size]);
+
+        count = count + 1;
+      });
+
+      next();
+    });
+  }, (err) => {
+    if (err) throw err;
+
+    console.log(table.toString());
+  });
+};
+
+module.exports.builder = {
+  l: {
+    alias: 'limit',
+    default: 1000,
+    describe: 'limit the # of groups to show (default 1000)'
+  },
+  r: {
+    alias: 'region',
+    describe: 'AWS Region to use',
+    default: 'us-east-1',
+  },
+  g: {
+    alias: 'group',
+    describe: 'Group you want to list',
+    default: 'prod-apps',
+  },
+};
+
+module.exports.handler = (cwlogs, argv) => {
+  getStreams(cwlogs, [argv.g]);
+};
---
+++ actions/tail.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-console */
+'use strict';
+const purdy = require('purdy');
+
+module.exports.builder = {
+  l: {
+    alias: 'limit',
+    default: 1000,
+    describe: 'limit the # of groups to show (default 1000)'
+  },
+  r: {
+    alias: 'region',
+    describe: 'AWS Region to use',
+    default: 'us-east-1',
+  },
+  g: {
+    alias: 'group',
+    describe: 'Group you want to list',
+    default: 'prod-apps',
+  },
+  s: {
+    alias: 'stream',
+    describe: 'Stream Name (optional)',
+    default: undefined,
+  },
+};
+
+const tail = (cwlogs, argv) => {
+  const logGroupName = argv.group;
+  const logStreamName = argv.stream;
+  if (logStreamName) {
+    console.log(' Tailing logs for group %s stream %s', logGroupName, logStreamName);
+  } else {
+    console.log(' Tailing logs for group %s ', logGroupName);
+  }
+  const initialParams = {
+    logGroupName,
+    interleaved: true
+  };
+
+  if (logStreamName) {
+    initialParams.logStreamNames = [logStreamName];
+  }
+
+  let count = 0;
+  const max = 300;
+  const defaultInterval = 5 * 1000;
+  const timePadding = 10 * 1000;
+  const seenEvents = {};
+
+  const getLogs = (params, startTime, limit) => {
+    params.startTime = new Date().getTime() - startTime - timePadding;
+
+    params.limit = limit || null;
+
+    cwlogs.filterLogEvents(params, (error, data) => {
+      if (error) {
+        console.log(error);
+      }
+
+      if (data.events.length !== 0) {
+        data.events.forEach((event) => {
+          if (seenEvents[event.eventId]) {
+            return;
+          }
+          const d = new Date(event.timestamp);
+          const localTime = d.toLocaleTimeString();
+          if (event.message[0] === '{') {
+            const json = JSON.parse(event.message);
+            json.localTime = localTime;
+            purdy(json);
+          } else {
+            console.log(`${localTime}: ${event.message}`);
+          }
+          seenEvents[event.eventId] = true;
+        });
+      }
+
+      params.nextToken = data.nextForwardToken;
+
+      count++;
+      if (count === max) {
+        console.log('--- All Done ---');
+        process.exit(0);
+      }
+      setTimeout(() => {
+        getLogs(params, defaultInterval);
+      }, defaultInterval);
+    });
+  };
+
+  getLogs(initialParams, 1000 * 60 * 30, 30);
+};
+
+module.exports.handler = (cwlogs, argv) => {
+  tail(cwlogs, argv);
+};
