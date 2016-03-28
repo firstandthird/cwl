5951532e926fba624f384075c86a40f849002c8b
chris <orthagonal1@gmail.com>
2016-03-28 14:52:13 -0500

    passes AWS.cloudwatch to each, etc

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 #!/usr/bin/env node
 'use strict';
+const _ = require('lodash');
+const yargs = require('yargs');
+const groups = require('./actions/groups.js')
+const streams = require('./actions/streams.js')
+const tail = require('./actions/tail.js')
 
-require('yargs')
-.command(
-  'groups',
-  'list cloudwatch log groups ',
-  require('./lib/groups.js')
-).command(
-  'streams',
-  'list streams for a group',
-  require('./lib/streams.js')
-).command(
-  'tail',
-  'grab the last 10 mins of logs and then fetch every few seconds',
-  require('./lib/tail.js')
-)
+const initAWS = (argv) => {
+  return require('./lib/commonAWS')({argv});
+};
+
+// major commands, each requires an inited interface to AWS.CloudWatch:
+const commands = [
+  {
+    name: 'groups',
+    desc: 'list cloudwatch log groups',
+    builder: groups.builder,
+    handler: (argv) => {
+      groups.handler(initAWS(argv), argv);
+    }
+  },
+  {
+    name: 'streams',
+    desc: 'list streams for a group',
+    builder: streams.builder,
+    handler: (argv) => {
+      streams.handler(initAWS(argv), argv);
+    }
+  },
+  {
+    name: 'tail',
+    desc: 'grab the last 10 mins of logs and then fetch every few seconds',
+    builder: tail.builder,
+    handler: (argv) => {
+      tail.handler(initAWS(argv), argv);
+    }
+  }
+];
+
+// register each command with yargs:
+_.each(commands, (c) => {
+  yargs.command(c.name, c.desc, c.builder, c.handler);
+});
+let usageString =  `$0 <command> where command is one of:
+
+`;
+// usageString += _.values(_.mapValues(commands, 'name')).join(', ');
+yargs
+.demand(1)
+.strict()
+// .usage(usageString)
 .help('h')
 .alias('h', 'help')
-.argv;
+.env('AWS_ACCESS_KEY_ID')
+.env('AWS_SECRET_ACCESS_KEY')
+yargs.argv;
