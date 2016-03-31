c1b497b074aa69cb96cd505c52b64ea32c63d3b5
chris <orthagonal1@gmail.com>
2016-03-30 21:39:11 -0500

    supports iterating backwards

--- actions/log.js
+++ actions/log.js
@@ -23,54 +23,115 @@ module.exports.builder = {
     describe: 'specify the streams to view',
     type: 'array'
   },
+  c: {
+    alias: 'chunk',
+    default: 1000,
+    describe: 'number of events to fetch at a time (adjust to adjust performance of log reader)'
+  }
 };
 
-const getNextPage = (cwlogs, argv, callback) => {
-  callback();
-};
+const stepSize = 1000 * 60 * 60;
+let curPage = [];
+let curParams = undefined;
 
-
-const getPrevPage = (cwlogs, argv, curParams, allDone) => {
-};
 
 const initParams = (argv) => {
   const params = {
-    logGroupName: argv.g,
-    limit: argv.l,
-  }
+    logGroupName: argv.g
+  };
+  // todo: let them specify a date range:
   // if (argv.b) {
   //   params.startTime = moment(argv.b).getTime();
   // } else {
   //   params.startTime = new Date().getTime() - (7 * 24 * 60 * 60 * 1000);
   // }
-  params.startTime = argv.b;
+  params.startTime = moment(new Date()).startOf('hour').toDate().getTime();
   if (argv.s.length > 0) {
     params.logStreamNames = argv.s;
   }
   return params;
 }
 
-let curPage = [];
-let curParams = undefined;
 
-const getStartingPage = (cwlogs, argv, allDone) => {
-  curParams = initParams(argv);
+const prevTime = () => {
+  console.log(`prev time is ${moment(curParams.endTime)}`);
+  curParams.startTime = moment(curParams.startTime).subtract(1, 'hours').toDate().getTime();
+  console.log(`post time is ${moment(curParams.startTime)}`);
+}
+const nextStartTime = () => {
+  curParams.startTime = moment(curParams.startTime).add(1, 'hours').toDate().getTime();
+}
+
+const getPrevPage = (cwlogs, argv, callback) => {
+  console.log("fetching prev page....");
+  if (curParams.nextToken) {
+    console.log("nextToken")
+    delete curParams.startTime;
+    delete curParams.endTime;
+    curPage = [];
+    getLogsUntilLimitReached(cwlogs, argv.l, prevTime, callback);
+  } else {
+    console.log("date limit")
+    curParams.endTime = _.last(curPage).timestamp;
+    curPage = [];
+    getLogsUntilLimitReached(cwlogs, argv.l, prevTime, callback);
+  }
+};
+
+
+const getNextPage = (cwlogs, argv, callback) => {
+  console.log("fetching next page...");
+  delete curParams.startTime;
+  delete curParams.endTime;
+  if (curParams.nextToken) {
+    curPage = [];
+    getLogsUntilLimitReached(cwlogs, argv.l, nextStartTime, callback);
+  } else {
+    curParams.endTime = _.last(curPage).timestamp;
+    curPage = [];
+    getLogsUntilLimitReached(cwlogs, argv.l, nextStartTime, callback);
+  }
+};
+
+
+const getLogsUntilLimitReached = (cwlogs, limit, iterator, allDone) => {
   async.whilst(
     () => {
-      return curPage.length < argv.l;
+      const notDone = curParams.nextToken || curPage.length < limit;
+      if (notDone === false) {
+        curPage = curPage.slice(0, limit);
+      }
+      return notDone;
     },
     (callback) => {
       cwlogs.filterLogEvents(curParams, (err, data) => {
         if (err) {
           return callback(err);
         }
-        curPage = _.union(curPage, data.events);
-        console.log(`building page, %s events...${curPage.length} `);
+        if (data.nextToken) {
+          console.log("there are more i didn't fetch");
+          curParams.nextToken = data.nextToken;
+        } else {
+          console.log("iterating date")
+          delete curParams.nextToken;
+          iterator();
+        }
+        curPage = _.sortBy(_.union(curPage, data.events), (o) => { return -o.timestamp;});
+        console.log(`building page, events fetched = ${curPage.length} `);
         callback();
       });
     },
     allDone
   );
+};
+
+const getStartingPage = (cwlogs, argv, allDone) => {
+  curParams = initParams(argv);
+  getLogsUntilLimitReached(cwlogs, argv.l, prevTime, allDone);
+};
+
+const formatTimestamp = (timestamp) => {
+  return moment(timestamp).format('MMMM Do, HH:mm:ss');
 };
 
 const printPageTable = (page) => {
@@ -79,7 +140,7 @@ const printPageTable = (page) => {
   })
   _.each(page, (event) => {
     table.push([
-      moment(event.timestamp).format('MM-DD HH:MM'),
+      formatTimestamp(event.timestamp),
       event.logStreamName,
       event.message
     ]);
@@ -89,7 +150,7 @@ const printPageTable = (page) => {
 
 const printPage = (page) => {
   _.each(page, (event) => {
-    console.log(`${event.logStreamName} ${moment(event.timestamp).format('MM-DD HH:MM').red}:  ${event.message.yellow}`
+    console.log(`${event.logStreamName} ${formatTimestamp(event.timestamp).red}  ${event.message.yellow}`
     );
   });
 }
@@ -117,11 +178,13 @@ module.exports.handler = (cwlogs, argv) => {
     }
     if (cmdMatches(prevCommands, result[promptMessage])) {
       return getPrevPage(cwlogs, argv, () => {
+        printPage(curPage);
         prompt.get([promptMessage], handlePrompt);
       });
     }
     if (cmdMatches(nextCommands, result[promptMessage])) {
       getNextPage(cwlogs, argv, () => {
+        printPage(curPage);
         prompt.get([promptMessage], handlePrompt);
       });
     }
