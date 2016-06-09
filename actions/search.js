f2c8ac21fc2e69ca0e124b67e0e455c7e15ece55
chris <orthagonal1@gmail.com>
2016-06-09 02:39:33 -0500

    uses curses-style log to display

--- actions/search.js
+++ actions/search.js
@@ -6,6 +6,12 @@ const moment = require('moment');
 const _ = require('lodash');
 const purdy = require('purdy');
 const logUtils = require('../lib/logUtils');
+const blessed = require('blessed');
+const contrib = require('blessed-contrib');
+
+let screen;
+let scrollableTable;
+const rows = [ ];
 
 module.exports.builder = {
   l: {
@@ -39,6 +45,10 @@ module.exports.builder = {
     default: false,
     describe: 'set to true to pretty-print as JSON, otherwise prints as table'
   },
+  scroll: {
+    default: false,
+    describe: 'set to true to print as a scrollable list'
+  },
   q: {
     alias: 'query',
     default: undefined,
@@ -46,9 +56,48 @@ module.exports.builder = {
   }
 };
 
+const printScrollableTable = (argv, logData) => {
+  screen = blessed.screen({
+    smartCSR: true
+  });
+  screen.key(['escape', 'q', 'C-c'], (ch, key) => {
+    return process.exit(0);
+  });
+  scrollableTable = contrib.table({
+    width: '80%',
+    height: '80%',
+    keys: true,
+    interactive: true,
+    label: 'Matching Log Events',
+    columnWidth: [20, 50, 20],
+    border: { type: "line", fg: "cyan" },
+    selectedFg: 'white',
+    selectedBg: 'blue',
+  });
+  scrollableTable.focus();
+  screen.append(scrollableTable);
+
+  screen.render();
+
+  _.each(logData.slice(0, argv.l), (log, count) => {
+    rows.push([
+      count,
+      `${log.logStreamName}: ${log.message}`,
+      moment(log.timestamp).format('YYYY-MM-DD HH:mm:SS')
+    ]);
+  });
+  scrollableTable.setData({
+    headers: ['Count', 'Msg', 'Timestamp'],
+    data: rows
+  });
+};
+
 const printLogSet = (argv, logData) => {
   if (logData.length === 0) {
     return;
+  }
+  if (argv.scroll) {
+    return printScrollableTable(argv, logData);
   }
   // sift messages by any query param:
   if (!argv.p) {
@@ -123,6 +172,7 @@ const getLogEventsForStream = (cwlogs, argv, streams, allDone) => {
 
 module.exports.handler = (cwlogs, argv) => {
   logUtils.startSpinner();
+
   async.auto({
     streams: (done) => {
       if (argv.s.length > 0) {
